599096d312cae20c053bc979371fb3b0a43e79ef
Aleksandar Belic <aleksandar.belic@netcare.de>
2018-08-20 09:56:45 +0200

    Generating tabula recta for Vigenere cipher.

--- src/cyphers/VigenereCipher.java
+++ src/cyphers/VigenereCipher.java
@@ -1,5 +1,8 @@
 package cyphers;
 
+import java.util.HashMap;
+import common.Constants;
+
 /**
  * VigenereCipher class.
  * 
@@ -7,9 +10,32 @@ package cyphers;
  *
  */
 public class VigenereCipher {
-
+	
+	private String alphabet = Constants.ALPHABET_EN;
+	private HashMap<Character, String> tabulaRecta = new HashMap<Character, String>();
+	
+	/**
+	 * Constructor generates tabula recta with default english alphabet.
+	 */
 	public VigenereCipher() {
-		
+		alphabet = Constants.ALPHABET_EN;
+		String shiftedAlphabet = "";
+		for (int i = 0; i < alphabet.length(); i++) {
+			shiftedAlphabet = alphabet.substring(i) + alphabet.substring(0, i);
+			tabulaRecta.put(alphabet.charAt(i), shiftedAlphabet);
+		}
+	}
+	
+	/**
+	 * Constructor generates tabula recta with user defined alphabet.
+	 */
+	public VigenereCipher(String userAlphabet) {
+		alphabet = userAlphabet;
+		String shiftedAlphabet = "";
+		for (int i = 0; i < alphabet.length(); i++) {
+			shiftedAlphabet = alphabet.substring(i) + alphabet.substring(0, i);
+			tabulaRecta.put(alphabet.charAt(i), shiftedAlphabet);
+		}
 	}
 	
 }
