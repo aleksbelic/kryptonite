9a5d73722ae38b3ec98d4ad67ebb51e31d724fd8
Aleksandar Belic <aleksandar.belic@netcare.de>
2018-08-23 10:10:44 +0200

    Starting rail fence cipher.

---
+++ src/cyphers/RailFenceCipher.java
@@ -0,0 +1,27 @@
+package cyphers;
+
+/**
+ * Rail Fence Cipher class.<br>
+ * Example for 3 rails:<br>
+ * <ul>
+ * <li>plaint text: WE ARE DISCOVERED FLEE AT ONCE</li>
+ * <li>cipher text: WECRLTEERDSOEEFEAOCAIVDEN</li>
+ * </ul>
+ * 
+ * @author Aleksandar Belic <aleks.belic@gmail.com>
+ */
+public class RailFenceCipher {
+
+	private int railCount = 2;
+
+	public RailFenceCipher(int railCount) {
+		this.railCount = railCount;
+	}
+
+	public String encrypt(String plainText) {
+		String cipherText = "";
+
+		return cipherText;
+	}
+
+}
