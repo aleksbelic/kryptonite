4909826dcfc3fec2bdffaa7c97b4b7a88b0078e6
Aleksandar Belic <aleks.belic@gmail.com>
2018-09-30 11:56:08 +0200

    Code refactoring, new tests added.

--- src/ciphers/ScytaleCipher.java
+++ src/ciphers/ScytaleCipher.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 public class ScytaleCipher {
 
 	private int diameter; // how many letters fit in a circle around rod
-
 	public ScytaleCipher(int diameter) {
 		this.diameter = diameter;
 	}
@@ -19,24 +18,22 @@ public class ScytaleCipher {
 	/**
 	 * Encrypts given string for specific rod diameter.
 	 * 
-	 * @param s string to encrypt
-	 * @return encrypted string
+	 * @param plaintext string to encrypt
+	 * @return ciphertext
 	 */
-	public String encrypt(String s) {
+	public String encrypt(String plaintext) {
 		StringBuilder encryptedStringStringBuilder = new StringBuilder();
 		ArrayList<Character> charArrayList = new ArrayList<>();
-		for (int i = 0; i < s.length(); i++) {
-			charArrayList.add(s.charAt(i));
+		for (int i = 0; i < plaintext.length(); i++) {
+			charArrayList.add(plaintext.charAt(i));
 		}
 		int i = 0;
-		while (encryptedStringStringBuilder.length() != s.length()) {
+		while (encryptedStringStringBuilder.length() != plaintext.length()) {
 			if (charArrayList.get(i) != null) {
 				encryptedStringStringBuilder.append(charArrayList.get(i));
 				charArrayList.set(i, null);
-				i = (i + (s.length() / diameter)) % s.length();
-			} else {
-				i++;
-			}
+				i = (i + (plaintext.length() / diameter)) % plaintext.length();
+			} else i++;
 		}
 		return encryptedStringStringBuilder.toString();
 	}
@@ -44,24 +41,21 @@ public class ScytaleCipher {
 	/**
 	 * Decrypts given string for specific rod diameter.
 	 * 
-	 * @param s string to decrypt
-	 * @return decrypted string
+	 * @param ciphertext string to decrypt
+	 * @return plaintext
 	 */
-	public String decrypt(String s) {
+	public String decrypt(String ciphertext) {
 		StringBuilder decryptedStringStringBuilder = new StringBuilder();
 		ArrayList<Character> charArrayList = new ArrayList<>();
-		for (int i = 0; i < s.length(); i++) {
-			charArrayList.add(s.charAt(i));
-		}
+		for (int i = 0; i < ciphertext.length(); i++)
+			charArrayList.add(ciphertext.charAt(i));
 		int i = 0;
-		while (decryptedStringStringBuilder.length() != s.length()) {
+		while (decryptedStringStringBuilder.length() != ciphertext.length()) {
 			if (charArrayList.get(i) != null) {
 				decryptedStringStringBuilder.append(charArrayList.get(i));
 				charArrayList.set(i, null);
-				i = (i + diameter) % s.length();
-			} else {
-				i++;
-			}
+				i = (i + diameter) % ciphertext.length();
+			} else i++;
 		}
 		return decryptedStringStringBuilder.toString();
 	}
--- src/tests/CeaserCipherTest.java
+++ src/tests/CeaserCipherTest.java
@@ -1,9 +1,14 @@
 package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ciphers.CaesarCipher;
 import common.Constants;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 /**
  * CeaserCipherTest class.
@@ -13,6 +18,18 @@ import common.Constants;
 class CeaserCipherTest {
 
 	private CaesarCipher cc = new CaesarCipher(Constants.ALPHABET_EN);
+	private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private PrintStream originalOut = System.out;
+
+	@BeforeEach
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+	}
+
+	@AfterEach
+	public void restoreStreams() {
+		System.setOut(originalOut);
+	}
 
 	@Test
 	void testEncryption() {
@@ -38,7 +55,13 @@ class CeaserCipherTest {
 		assertEquals(cc.decryptTwoKeys("Myvx wrqh vmopb Pivwdkh.", 3, 4), "Just some silly Message.");
 	}
 
-	// TODO eyeball
+	@Test
+	void testEyeball() {
+		cc.eyeball("Lujyfwa tl.");
+		// Win (\r\n)
+		assertEquals(outContent.toString(),"Ktixevz sk.\r\nJshwduy rj.\r\nIrgvctx qi.\r\nHqfubsw ph.\r\nGpetarv og.\r\nFodszqu nf.\r\nEncrypt me.\r\nDmbqxos ld.\r\nClapwnr kc.\r\nBkzovmq jb.\r\nAjynulp ia.\r\nZixmtko hz.\r\nYhwlsjn gy.\r\nXgvkrim fx.\r\nWfujqhl ew.\r\nVetipgk dv.\r\nUdshofj cu.\r\nTcrgnei bt.\r\nSbqfmdh as.\r\nRapelcg zr.\r\nQzodkbf yq.\r\nPyncjae xp.\r\nOxmbizd wo.\r\nNwlahyc vn.\r\nMvkzgxb um.\r\nLujyfwa tl.\r\n");
+	}
+
 	// TODO eyeballTwoKeys
 
 }
