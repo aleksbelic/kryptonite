ddd3a4c1158b81219d13fecbe121c3a48f95d9ab
Aleksandar Belic <aleks.belic@gmail.com>
2019-04-22 02:27:04 +0200

    Porta Cipher - done. New exception handling, new tests.

--- src/ciphers/PortaCipher.java
+++ src/ciphers/PortaCipher.java
@@ -7,6 +7,22 @@ import common.Constants;
  * The Porta Cipher is a polyalphabetic substitution cipher invented by Giambattista della Porta.
  * It uses only 13 alphabets so that the first half (A-M) is reciprocal with the second half.
  *
+ * Keys  A B C D E F G H I J K L M
+ * -------------------------------
+ * A,B 	 N O P Q R S T U V W X Y Z
+ * C,D 	 O P Q R S T U V W X Y Z N
+ * E,F 	 P Q R S T U V W X Y Z N O
+ * G,H 	 Q R S T U V W X Y Z N O P
+ * I,J 	 R S T U V W X Y Z N O P Q
+ * K,L 	 S T U V W X Y Z N O P Q R
+ * M,N 	 T U V W X Y Z N O P Q R S
+ * O,P 	 U V W X Y Z N O P Q R S T
+ * Q,R 	 V W X Y Z N O P Q R S T U
+ * S,T 	 W X Y Z N O P Q R S T U V
+ * U,V 	 X Y Z N O P Q R S T U V W
+ * W,X 	 Y Z N O P Q R S T U V W X
+ * Y,Z 	 Z N O P Q R S T U V W X Y
+ *
  * @author Aleksandar Belic <aleks.belic@gmail.com>
  */
 public class PortaCipher {
@@ -20,7 +36,11 @@ public class PortaCipher {
 	 */
 	public String encrypt(String plaintext, String keyword) {
 
-		plaintext = plaintext.replaceAll("\\s+","");
+		if (keyword.isEmpty())
+			throw new IllegalArgumentException("ERROR: keyword cannot be an empty string.");
+
+		// removing whitespace
+		plaintext = plaintext.replaceAll("\\s+", "");
 
 		String alphabet = Constants.ALPHABET_EN;
 		StringBuilder plaintextStringBuilder = new StringBuilder();
@@ -31,15 +51,18 @@ public class PortaCipher {
 
 			int currentKeywordCharIndexInAlphabet = alphabet.indexOf(currentKeywordChar);
 			if (currentKeywordCharIndexInAlphabet == -1) {
-				throw new IllegalArgumentException("ERROR: keyword can contain only English alphabet letters, no whitespace.");
-			}
-			if (currentKeywordCharIndexInAlphabet != -1) {
+				throw new IllegalArgumentException("ERROR: keyword can contain only English alphabet letters, no whitespace, numbers or special characters.");
+			} else {
 				int keywordLetterGroup = currentKeywordCharIndexInAlphabet / 2;
 				String keywordLetterGroupAlphabet = alphabet.substring(0, 13) + alphabet.substring(13).substring(keywordLetterGroup) + alphabet.substring(13).substring(0, keywordLetterGroup);
 
-				int substitutionCharIndex = (keywordLetterGroupAlphabet.indexOf(currentChar) + 13) % alphabet.length();
-				plaintextStringBuilder.append(keywordLetterGroupAlphabet.charAt(substitutionCharIndex));
-
+				int currentCharPositionInKeywordLetterGroupAlphabet = keywordLetterGroupAlphabet.indexOf(currentChar);
+				if (currentCharPositionInKeywordLetterGroupAlphabet == -1) {
+					throw new IllegalArgumentException("ERROR: Plaintext & ciphertext can contain only English alphabet letters, no numbers or special characters allowed.");
+				} else {
+					int substitutionCharIndex = (currentCharPositionInKeywordLetterGroupAlphabet + 13) % keywordLetterGroupAlphabet.length();
+					plaintextStringBuilder.append(keywordLetterGroupAlphabet.charAt(substitutionCharIndex));
+				}
 			}
 		}
 		return plaintextStringBuilder.toString();
@@ -50,7 +73,7 @@ public class PortaCipher {
 	 * Decrypts given string using specified keyword.
 	 *
 	 * @param ciphertext string to decrypt
-	 * @param keyword   phrase to use in decryption process
+	 * @param keyword    phrase to use in decryption process
 	 * @return plaintext
 	 */
 	public String decrypt(String ciphertext, String keyword) {
--- src/tests/PortaCipherTest.java
+++ src/tests/PortaCipherTest.java
@@ -12,19 +12,61 @@ public class PortaCipherTest {
 
 	@Test
 	void testEncryption() {
+		assertEquals(pc.encrypt("", "key"), "");
 		assertEquals(pc.encrypt("Meetmeatthebridgeatfive", "key"), "RTQBOQSEHZTNMXPYTZBUUDT");
 		assertEquals(pc.encrypt("Meet me at the bridge at five", "key"), "RTQBOQSEHZTNMXPYTZBUUDT");
+		assertEquals(pc.encrypt("The Porta Cipher is a polyalphabetic substitution cipher invented by Giambattista della Porta", "someStrongKeyword"), "KOXAFILUVYKWQGPKOGHRJWUHOTRWEUNLMPJMOELKQHHSNATPKQMMYHENZWEZYSONYMLWJMTSNUTUJLMEZ");
+
+		// plaintext exceptions
 		assertThrows(IllegalArgumentException.class, () -> {
-			pc.encrypt("Some plaintext", "keyword with whitespace"); // whitespace in keyword
+			pc.encrypt("Some_plaintext_with_special_character!", "KEY"); // plaintext contains special char
 		});
 		assertThrows(IllegalArgumentException.class, () -> {
-			pc.encrypt("Some plaintext", "k3yw0rd_w1th_non-english_char"); // non-english char
+			pc.encrypt("Some plaintext with numbers 123", "KEY"); // plaintext contains number
+		});
+
+		// keyword exceptions
+		assertThrows(IllegalArgumentException.class, () -> {
+			pc.encrypt("Some plaintext", ""); // keyword is empty string
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			pc.encrypt("Some plaintext", "keyword with whitespace"); // keyword contains whitespace
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			pc.encrypt("Some plaintext", "keyword_with_special_char!"); // keyword contains special char
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			pc.encrypt("Some plaintext", "abc123"); // keyword contains number
 		});
 	}
 
 	@Test
 	void testDecryption() {
-		//assertEquals(pc.decrypt("RTQB OQ SE HZT NMXPYT ZB UUDT", ""), "MEET ME AT THE BRIDGE AT FIVE");
+		assertEquals(pc.decrypt("", "key"), "");
+		assertEquals(pc.decrypt("RTQBOQSEHZTNMXPYTZBUUDT", "key"), "MEETMEATTHEBRIDGEATFIVE");
+		assertEquals(pc.decrypt("KOXAFILUVYKWQGPKOGHRJWUHOTRWEUNLMPJMOELKQHHSNATPKQMMYHENZWEZYSONYMLWJMTSNUTUJLMEZ", "someStrongKeyword"), "THEPORTACIPHERISAPOLYALPHABETICSUBSTITUTIONCIPHERINVENTEDBYGIAMBATTISTADELLAPORTA");
+
+		// ciphertext exceptions
+		assertThrows(IllegalArgumentException.class, () -> {
+			pc.encrypt("Some_ciphertext_with_special_character!", "KEY"); // ciphertext contains special char
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			pc.encrypt("Some ciphertext with numbers 123", "KEY"); // cipher contains number
+		});
+
+		// keyword exceptions
+		assertThrows(IllegalArgumentException.class, () -> {
+			pc.decrypt("Some ciphertext", ""); // keyword is empty string
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			pc.decrypt("Some ciphertext", "keyword with whitespace"); // keyword contains whitespace
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			pc.decrypt("Some ciphertext", "keyword_with_special_char!"); // keyword contains special char
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			pc.decrypt("Some ciphertext", "abc123"); // keyword contains number
+		});
 	}
 
 }
